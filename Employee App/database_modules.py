3561b2f11ec85a1d9c2ace8a1ce2a8f01aab9c14
SunniM <sunnismagan@gmail.com>
2025-11-19 09:27:20 -0600

    added Employee app that works with pandas DFs

---
+++ Employee App/database_modules.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from datetime import date
+import sqlite3
+class Database:
+    def __init__(self):
+        self.user_df = pd.read_csv("../users.csv")
+        self.user_df["isManager"] = self.user_df["isManager"].astype(bool)
+
+        self.expenses_df = pd.read_csv("../expenses.csv")
+        self.expenses_df.set_index("id", inplace=True)
+
+    def validate_user(self, username, password):
+        users = self.user_df.loc[(self.user_df['username'] == username) & (self.user_df['password'] == password)]
+        if not users.empty:
+            return True
+        return False
+
+    def add(self, new_date, amount, category, description):
+        new_date = new_date.split("-")
+        year, month, day = [int(x) for x in new_date]
+        # "id": self.expenses_df['id'].max() + 1,
+        entry = pd.Series({"date": date(year, month, day).__str__(), "amount": amount, "category": category, "description": description})
+        self.expenses_df = pd.concat([self.expenses_df, entry.to_frame().T], ignore_index=True)
+        self.view()
+
+    def view(self):
+        print(self.expenses_df.to_string())
+        print()
+
+    def update(self, index, new_date, amount, category, description):
+        new_date = new_date.split("-")
+        year, month, day = [int(x) for x in new_date]
+        print(self.expenses_df.loc[(self.user_df['id'] == index)])
+        self.expenses_df.loc[(self.user_df['id'] == index)] = [index, date(year, month, day).__str__(), amount, category, description]
+        print("TEST: ", self.expenses_df.loc[index])
+        self.view()
+
+    def close(self):
+        self.expenses_df.to_csv("../expenses.csv", index_label='id')
